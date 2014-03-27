1da3cc82ea28508f27e20c4b68ef3ea56624512c
Ostrenkiy <x1te.rus@gmail.com>
2014-03-27 22:54:23 +0400

    Coefficients
    Added coefficients, possibly will delete powers from 5th or 6th

--- CourseWork/FormWithResults.Designer.cs
+++ CourseWork/FormWithResults.Designer.cs
@@ -30,17 +30,20 @@
         {
             this.tabControlCases = new System.Windows.Forms.TabControl();
             this.tabPageWorst = new System.Windows.Forms.TabPage();
-            this.tabPageAverage = new System.Windows.Forms.TabPage();
             this.dataGridViewWorst = new System.Windows.Forms.DataGridView();
-            this.tabPageBest = new System.Windows.Forms.TabPage();
+            this.tabPageAverage = new System.Windows.Forms.TabPage();
             this.dataGridViewAverage = new System.Windows.Forms.DataGridView();
+            this.tabPageBest = new System.Windows.Forms.TabPage();
             this.dataGridViewBest = new System.Windows.Forms.DataGridView();
+            this.labelWorst = new System.Windows.Forms.Label();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.labelBest = new System.Windows.Forms.Label();
             this.tabControlCases.SuspendLayout();
             this.tabPageWorst.SuspendLayout();
-            this.tabPageAverage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWorst)).BeginInit();
-            this.tabPageBest.SuspendLayout();
+            this.tabPageAverage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAverage)).BeginInit();
+            this.tabPageBest.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBest)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,6 +60,7 @@
             // 
             // tabPageWorst
             // 
+            this.tabPageWorst.Controls.Add(this.labelWorst);
             this.tabPageWorst.Controls.Add(this.dataGridViewWorst);
             this.tabPageWorst.Location = new System.Drawing.Point(4, 22);
             this.tabPageWorst.Name = "tabPageWorst";
@@ -67,17 +71,6 @@
             this.tabPageWorst.UseVisualStyleBackColor = true;
             this.tabPageWorst.Click += new System.EventHandler(this.tabPageWorst_Click);
             // 
-            // tabPageAverage
-            // 
-            this.tabPageAverage.Controls.Add(this.dataGridViewAverage);
-            this.tabPageAverage.Location = new System.Drawing.Point(4, 22);
-            this.tabPageAverage.Name = "tabPageAverage";
-            this.tabPageAverage.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageAverage.Size = new System.Drawing.Size(835, 348);
-            this.tabPageAverage.TabIndex = 1;
-            this.tabPageAverage.Text = "Средний случай";
-            this.tabPageAverage.UseVisualStyleBackColor = true;
-            // 
             // dataGridViewWorst
             // 
             this.dataGridViewWorst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -87,15 +80,17 @@
             this.dataGridViewWorst.Size = new System.Drawing.Size(823, 86);
             this.dataGridViewWorst.TabIndex = 0;
             // 
-            // tabPageBest
+            // tabPageAverage
             // 
-            this.tabPageBest.Controls.Add(this.dataGridViewBest);
-            this.tabPageBest.Location = new System.Drawing.Point(4, 22);
-            this.tabPageBest.Name = "tabPageBest";
-            this.tabPageBest.Size = new System.Drawing.Size(835, 348);
-            this.tabPageBest.TabIndex = 2;
-            this.tabPageBest.Text = "Лучший случай";
-            this.tabPageBest.UseVisualStyleBackColor = true;
+            this.tabPageAverage.Controls.Add(this.labelAverage);
+            this.tabPageAverage.Controls.Add(this.dataGridViewAverage);
+            this.tabPageAverage.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAverage.Name = "tabPageAverage";
+            this.tabPageAverage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAverage.Size = new System.Drawing.Size(835, 348);
+            this.tabPageAverage.TabIndex = 1;
+            this.tabPageAverage.Text = "Средний случай";
+            this.tabPageAverage.UseVisualStyleBackColor = true;
             // 
             // dataGridViewAverage
             // 
@@ -106,6 +101,17 @@
             this.dataGridViewAverage.Size = new System.Drawing.Size(823, 86);
             this.dataGridViewAverage.TabIndex = 1;
             // 
+            // tabPageBest
+            // 
+            this.tabPageBest.Controls.Add(this.labelBest);
+            this.tabPageBest.Controls.Add(this.dataGridViewBest);
+            this.tabPageBest.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBest.Name = "tabPageBest";
+            this.tabPageBest.Size = new System.Drawing.Size(835, 348);
+            this.tabPageBest.TabIndex = 2;
+            this.tabPageBest.Text = "Лучший случай";
+            this.tabPageBest.UseVisualStyleBackColor = true;
+            // 
             // dataGridViewBest
             // 
             this.dataGridViewBest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -114,6 +120,36 @@
             this.dataGridViewBest.RowHeadersWidth = 150;
             this.dataGridViewBest.Size = new System.Drawing.Size(823, 86);
             this.dataGridViewBest.TabIndex = 1;
+            // 
+            // labelWorst
+            // 
+            this.labelWorst.AutoSize = true;
+            this.labelWorst.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWorst.Location = new System.Drawing.Point(3, 92);
+            this.labelWorst.Name = "labelWorst";
+            this.labelWorst.Size = new System.Drawing.Size(35, 13);
+            this.labelWorst.TabIndex = 1;
+            this.labelWorst.Text = "label1";
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAverage.Location = new System.Drawing.Point(3, 92);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(51, 20);
+            this.labelAverage.TabIndex = 2;
+            this.labelAverage.Text = "label1";
+            // 
+            // labelBest
+            // 
+            this.labelBest.AutoSize = true;
+            this.labelBest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBest.Location = new System.Drawing.Point(3, 92);
+            this.labelBest.Name = "labelBest";
+            this.labelBest.Size = new System.Drawing.Size(51, 20);
+            this.labelBest.TabIndex = 2;
+            this.labelBest.Text = "label1";
             // 
             // FormWithResults
             // 
@@ -126,10 +162,13 @@
             this.Load += new System.EventHandler(this.FormWithResults_Load);
             this.tabControlCases.ResumeLayout(false);
             this.tabPageWorst.ResumeLayout(false);
-            this.tabPageAverage.ResumeLayout(false);
+            this.tabPageWorst.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWorst)).EndInit();
-            this.tabPageBest.ResumeLayout(false);
+            this.tabPageAverage.ResumeLayout(false);
+            this.tabPageAverage.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAverage)).EndInit();
+            this.tabPageBest.ResumeLayout(false);
+            this.tabPageBest.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBest)).EndInit();
             this.ResumeLayout(false);
 
@@ -144,6 +183,9 @@
         private System.Windows.Forms.TabPage tabPageBest;
         private System.Windows.Forms.DataGridView dataGridViewAverage;
         private System.Windows.Forms.DataGridView dataGridViewBest;
+        private System.Windows.Forms.Label labelWorst;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.Label labelBest;
 
     }
 }
--- CourseWork/FormWithResults.cs
+++ CourseWork/FormWithResults.cs
@@ -11,6 +11,7 @@ namespace CourseWork
 {
     public partial class FormWithResults : Form
     {
+        string[] symbols = { Char.ConvertFromUtf32(0x2070), Char.ConvertFromUtf32(0x00B9), Char.ConvertFromUtf32(0x00B2), Char.ConvertFromUtf32(0x00B3), Char.ConvertFromUtf32(0x2074), Char.ConvertFromUtf32(0x2075), Char.ConvertFromUtf32(0x2076), Char.ConvertFromUtf32(0x2077), Char.ConvertFromUtf32(0x2078), Char.ConvertFromUtf32(0x2079) };
         TestingResults data;
         public FormWithResults(TestingResults tRes)
         {
@@ -32,19 +33,44 @@ namespace CourseWork
             }
         }
 
+        string MakeSuperscriptNumber(int x)
+        {
+            string xString = x.ToString();
+            string res = "";
+            for (int i = 0; i < xString.Length; i++)
+            {
+                res += symbols[Int32.Parse(xString[i]+"")];
+            }
+            return res;
+
+        }
+
+        void InitializeLabel(Label lb, double[] coeff, int n)
+        {
+            lb.Text = "";
+            string format = "{0:F6}*x{1} + ";
+            for (int i = 0; i < n; i++)
+            {
+                lb.Text += String.Format(format, coeff[i], MakeSuperscriptNumber(i));
+            }
+        }
+
         void InitializeWorst()
         {
             InitializeDataGrid(dataGridViewWorst, data.worst, data.n);
+            InitializeLabel(labelWorst, data.coeffWorst, data.n);
         }
 
         void InitializeAverage()
         {
             InitializeDataGrid(dataGridViewAverage, data.average, data.n);
+            InitializeLabel(labelAverage, data.coeffAverage, data.n);
         }
 
         void InitializeBest()
         {
             InitializeDataGrid(dataGridViewBest, data.best, data.n);
+            InitializeLabel(labelBest, data.coeffBest, data.n);
         }
 
         private void InitializeCases()
